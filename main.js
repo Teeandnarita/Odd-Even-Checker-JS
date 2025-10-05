ef2ef847215da438a07c70a79f5b82a689e8ea70
Teeandnarita <128231239+Teeandnarita@users.noreply.github.com>
2025-10-05 09:00:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var a = prompt("Enter a number")
+var a = prompt("Enter a number");
 
 function check() {
-if(a / 2 == 0){
-console.log("This is a even number")
-}else {
-console.log("This is a Odd Number")
-}
+  if (a / 2 == 0) {
+    console.log("This is a even number");
+  } else {
+    console.log("This is a Odd Number");
+  }
 }
 
-check ()
+check();
